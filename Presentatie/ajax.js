a7a3dcb8bb8d918a4f783846a74fc80d8db3bd77
forenzer <marinus_eekman@hotmail.com>
2014-10-23 21:57:28 +0200

    Presentatie Gemaakt
    Presentatie voor de tussentijdse evaluatie gemaakt. Deze maakt gebruik
    van php.

---
+++ Presentatie/ajax.js
@@ -0,0 +1,67 @@
+$(document).ready(function()
+{  
+    var sfield = $("#s");  
+    var container = $("#photos");  
+    var timer;
+    
+    $("#TextBlocks div").hide();
+    $("#TextBlocks .voorstelling").slideDown();
+    
+    $("#menu a").click(function(eEvent)
+	{
+        eEvent.preventDefault();
+        thisName = $(this).attr("id");
+        $("#TextBlocks div:not(." + thisName + ")").slideUp();
+        $("#TextBlocks ." + thisName).slideDown();
+    });
+
+    $(sfield).keydown(function(e)
+    {  
+        if(e.keyCode == '32' || e.keyCode == '188' || e.keyCode == '189' || e.keyCode == '13' || e.keyCode == '190' || e.keyCode == '219' || e.keyCode == '221' || e.keyCode == '191' ||    e.keyCode == '220')
+        {  
+            e.preventDefault();  
+        } 
+        else 
+        {  
+            clearTimeout(timer);  
+      
+            timer = setTimeout(function() 
+            {  
+                instaSearch();  
+            }, 900);     
+        }  
+    });
+    function instaSearch() 
+    {  
+        $(sfield).addClass("loading");  
+        $(container).empty();  
+        var q = $(sfield).val();  
+      
+        $.ajax
+        ({  
+            type: 'POST',  
+            url: 'instasearch.php',  
+            data: "q="+q,  
+            success: function(data)
+            {  
+                $(sfield).removeClass("loading");
+                
+                console.log(data);
+              
+                $.each(data, function(i, item) 
+                {  
+                    var ncode = '<a rel="external" href="'+data[i].url+'" target="_blank"><img src="'+data[i].thumb+'"></a></div>';
+                    //ncode +='<div class="p"><a rel="external" href="'+data[i].url+'" class="fullsize" target="_blank"><img src="'+data[i].src+'" alt="fullsize"></a>';
+                    //var ncode = '<p>'+data[i].thumb+'</p>';
+                    $(container).append(ncode); 
+                }); 
+                
+            },  
+                error: function(xhr, type, exception) 
+                {   
+                    $(sfield).removeClass("loading");  
+                    $(container).html("Error: " + type);   
+                }  
+        });  
+    }
+});
