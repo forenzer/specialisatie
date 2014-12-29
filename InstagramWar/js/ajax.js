1ea3bc92446492204cb6b5a1d18489942c76df06
forenzer <marinus_eekman@hotmail.com>
2014-12-29 19:11:35 +0100

    Tweede succesvolle Ajax - call

--- InstagramWar/js/ajax.js
+++ InstagramWar/js/ajax.js
@@ -11,8 +11,38 @@ $(document).ready(function()
         url: 'https://api.instagram.com/v1/users/search/?q=' + $user_input1 + '&access_token=' + $hidden,
         dataType: "jsonp",
         success: function(json)
-        {    
-            console.log('SUCCESS!');
+        {   
+            $count = 1; //opzettelijk op 1 gezet, op deze manier spaar ik lijntjes code uit.
+            for(iUser in json.data)
+            {
+                oUser = json.data[iUser];
+                
+                getUserValues(oUser.id, $hidden);
+                
+                if($count > 0){break;}
+            }
         }
     }); 
+    
+    function getUserValues($user_id, $access_token)
+    {
+        $.ajax
+        ({
+            url: 'https://api.instagram.com/v1/users/' + $user_id + '/?access_token=' + $access_token,
+            dataType: "jsonp",
+            success: function(json)
+            {
+                //console.log(json.data);
+                $count = 1; //opzettelijk op 1 gezet, op deze manier spaar ik lijntjes code uit.
+                for(iValue in json.data)
+                {
+                    oValue = json.data[iValue];
+                    
+                    //console.log(oValue);
+                    
+                    if($count > 0){break;}
+                }
+            }
+        });
+    }          
 });
